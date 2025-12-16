12b4e171d9726db9cf74ec869f2a33759152e0af
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-16 14:14:04 -0600

    Add todos array and markAsDone function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let todos = ['first todo', 'second todo', 'third todo'];
+let count = 0;
 
+function markAsDone() {
+  while(count < 3) {
+    todos[count] = 'done - ' + todos[count];
+    ++count;
+  }
+}
